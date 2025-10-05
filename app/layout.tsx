f105a789a66c271d6d3929adebb9514f5c39b210
Leo Gall <leo@lgll.dev>
2025-10-05 09:36:55 +0200

    feat: redirect from poxc notice

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,12 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body className="antialiased">{children}</body>
-      <div className="absolute right-3 bottom-3 flex items-center gap-2 text-gray-600">
+      <a
+        href="https://politicalxchange.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="absolute right-3 bottom-3 flex items-center gap-2 text-gray-600"
+      >
         Made with ❤️ by Political X Change
         <img
           src="/poxc.webp"
@@ -23,7 +28,7 @@ export default function RootLayout({
           width={20}
           height={20}
         />
-      </div>
+      </a>
     </html>
   );
 }
