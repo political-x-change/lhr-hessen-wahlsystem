ffd3d5e431803a0acac01ffea2d50198cb6ec5ed
Leo Gall <leo@lgll.dev>
2025-10-19 10:03:11 +0200

    fix: repair tests by changing the file type of the jest config to `.mjs`

---
+++ jest.config.mjs
@@ -0,0 +1,49 @@
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config = {
+  coverageProvider: "v8",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: [
+    "**/__tests__/**/*.test.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[jt]s?(x)",
+  ],
+  collectCoverageFrom: [
+    "lib/**/*.{js,jsx,ts,tsx}",
+    "app/**/*.{js,jsx,ts,tsx}",
+    "components/**/*.{js,jsx,ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/.next/**",
+  ],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  projects: [
+    {
+      displayName: "api",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/__tests__/app/api/**/*.test.[jt]s?(x)"],
+      preset: "ts-jest",
+      moduleNameMapper: { "^@/(.*)$": "<rootDir>/$1" },
+    },
+    {
+      displayName: "lib",
+      testEnvironment: "node",
+      testMatch: ["<rootDir>/__tests__/lib/**/*.test.[jt]s?(x)"],
+      preset: "ts-jest",
+      moduleNameMapper: { "^@/(.*)$": "<rootDir>/$1" },
+    },
+    {
+      displayName: "components",
+      testEnvironment: "jsdom",
+      setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+      testMatch: ["<rootDir>/__tests__/components/**/*.test.[jt]s?(x)"],
+      preset: "ts-jest",
+      moduleNameMapper: { "^@/(.*)$": "<rootDir>/$1" },
+    },
+  ],
+};
+
+export default createJestConfig(config);
